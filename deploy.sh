58f2be298ad6563164c9cec0861028f84f8822d9
odinuv <odinuv@gmail.com>
2017-11-25 17:54:33 +0100

    fix: app name

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ set -e
 
 # deploy to Quay public repository
 docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
-docker tag keboola/generic-extractor quay.io/keboola/generic-extractor:${TRAVIS_TAG}
-docker tag keboola/generic-extractor quay.io/keboola/generic-extractor:latest
+docker tag mock-server quay.io/keboola/generic-extractor:${TRAVIS_TAG}
+docker tag mock-server quay.io/keboola/generic-extractor:latest
 docker images
 docker push quay.io/keboola/generic-extractor:${TRAVIS_TAG}
 docker push quay.io/keboola/generic-extractor:latest
